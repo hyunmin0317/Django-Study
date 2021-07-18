b8383a5827987d4583d1702add8bad8f877bb061
hyunmin0317 <choihm9903@naver.com>
2021-07-18 11:44:04 +0900

    Update Jump-to-Django chap2-04

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
+# pybo 목록 출력
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+ #pybo 내용 출력
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
