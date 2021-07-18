20320bf36a9b63b64e31452d65057441590b5107
hyunmin0317 <choihm9903@naver.com>
2021-07-18 15:13:59 +0900

    Update Jump-to-Django chap2

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -12,3 +12,6 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.content
