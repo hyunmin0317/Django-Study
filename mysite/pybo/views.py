a082a98ffeaaa6ab015bddfdad568cc1ca20806c
hyunmin0317 <choihm9903@naver.com>
2021-07-20 14:28:00 +0900

    Update chap3-02

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Question
 from .forms import QuestionForm, AnswerForm
 
 # pybo 목록 출력
 def index(request):
+    page = request.GET.get('page', '1')  # 페이지
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 # pybo 내용 출력
