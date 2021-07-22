1e05128aa30f4806bf47c9d1779bacd02940479a
hyunmin0317 <choihm9903@naver.com>
2021-07-22 14:18:17 +0900

    Update chap3-08

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -6,6 +6,7 @@ class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.subject
@@ -15,6 +16,7 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.content
