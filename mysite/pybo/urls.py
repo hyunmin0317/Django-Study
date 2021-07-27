35c578d55035a1622030124244a06d1fd2597119
hyunmin0317 <choihm9903@naver.com>
2021-07-27 19:44:29 +0900

    Update chap2-04

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'pybo'
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('<int:question_id>/', views.detail),
 ]
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
+# pybo 목록 출력
 def index(request):
-    return render(request, 'pybo/index.html')
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+# pybo 내용 출력
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question':question}
+    return render(request, 'pybo/question_detail.html', context)
