6d360e0792f4ad121494bff7fbf5c4aaa36004cc
hyunmin0317 <choihm9903@naver.com>
2021-07-30 16:03:08 +0900

    Update chap3-08

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -4,6 +4,7 @@ from .models import Question
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # pybo 목록 출력
 def index(request):
