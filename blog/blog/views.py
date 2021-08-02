5d658f1fb019668d7bfa9180bd951484b1a137e4
hyunmin0317 <choihm9903@naver.com>
2021-08-02 21:01:32 +0900

    Update blog

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Blog
+from .forms import BlogPost
 
 def home(request):
     blogs = Blog.objects
-    content = {'blogs':blogs}
+    # 블로그 모든 글들을 대상으로
+    blog_list = Blog.objects.all()
+    # 블로그 객체 세 개를 한 페이지로 자르기
+    paginator = Paginator(blog_list, 3)
+    # request된 페이지가 뭔지를 알아내고 ( request페이지를 변수에 담아냄 )
+    page = request.GET.get('page')
+    # request된 페이지를 얻어온 뒤 return 해 준다
+    posts = paginator.get_page(page)
+
+    content = {'blogs': blogs, 'posts': posts}
     return render(request, 'blog/home.html', content)
 
 def detail(request, blog_id):
@@ -22,3 +33,15 @@ def create(request):
     blog.pub_date = timezone.datetime.now()
     blog.save()
     return redirect('/blog/'+str(blog.id))
+
+def blogpost(request):
+    if request.method =='POST':
+        form = BlogPost(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.pub_date=timezone.now()
+            post.save()
+            return redirect('home')
+    else:
+        form = BlogPost()
+        return render(request,'blog/form.html',{'form':form})
