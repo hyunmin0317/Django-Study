2fa02b5b2b993e59442a606661fcdd7ab622c475
hyunmin0317 <choihm9903@naver.com>
2021-08-02 21:11:45 +0900

    Update

--- blog/config/settings.py
+++ blog/config/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tf*&@&9g2%#i1sfj__^!28-*si^(ohe*seg1q$oeuen=qm9iio'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
